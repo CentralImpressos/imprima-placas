fedc38a31f906c578b9c5a0c434f6877648768a9
CentralImpressos <centralimpressosvendas@gmail.com>
2026-09-14 14:19:52 -0300

    Adiciona cor CMYK para círculo e proibição

--- src/types/index.ts
+++ src/types/index.ts
@@ -19,6 +19,7 @@ export type SignAppearance = {
   frameColor: CmykColor;
   backgroundColor: CmykColor;
   iconColor: CmykColor;
+  symbolColor: CmykColor;
   frameEnabled: boolean;
   circle: boolean;
   prohibition: boolean;
