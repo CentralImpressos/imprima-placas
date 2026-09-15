91515937dd411dc33076f3fdedaa791f627770c4
CentralImpressos <centralimpressosvendas@gmail.com>
2026-09-15 14:47:09 +0000

    Ajustando pictograma de cigarro.

--- src/data/presets.ts
+++ src/data/presets.ts
@@ -17,7 +17,7 @@ export const SIGN_PRESETS: SignPreset[] = [
     name: 'Proibido Fumar',
     frameType: 'simple',
     message: 'PROIBIDO\nFUMAR',
-    icon: 'mdi:smoking',
+    icon: 'streamline-plump:smoking-area-solid',
     defaults: { circle: true, prohibition: true, frameColor: DEFAULT_FRAME_COLOR, pictogramPosition: 'top' },
   },
   {
